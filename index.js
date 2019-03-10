c1261281747b83067c1ce02f71f904eee19b6dcc
Yulia Karapetyants <yulia.karapetyants@gmail.com>
2019-03-10 12:27:55 +0200

    dz13

---
+++ index.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+fs.writeFile('data.txt', 'new text!', (err, data) => {
+    if (err) throw err;   
+});
+
+fs.appendFile('data.txt', '\nanother text', (err, data) => {
+    if (err) throw err;
+    fs.readFile('data.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    }); 
+});
