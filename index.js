3d3b0488881ef320294593c0dce1f1c4b9a81814
Yulia Karapetyants <yulia.karapetyants@gmail.com>
2019-03-10 12:33:48 +0200

    remove unused param

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 
-fs.writeFile('data.txt', 'new text!', (err, data) => {
+fs.writeFile('data.txt', 'new text!', (err) => {
     if (err) throw err;   
 });
 
-fs.appendFile('data.txt', '\nanother text', (err, data) => {
+fs.appendFile('data.txt', '\nanother text', (err) => {
     if (err) throw err;
     fs.readFile('data.txt', 'utf8', (err, data) => {
         if (err) throw err;
