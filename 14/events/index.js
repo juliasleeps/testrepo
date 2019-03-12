64bc902be6422e7a007586482a4d36f206fd3ef5
Yuka <cas_siopeia@live.com>
2019-03-12 21:25:52 +0200

    events added

--- 14/events/index.js
+++ 14/events/index.js
@@ -1,12 +1,36 @@
+// var Event = require('events').EventEmitter;
+
+// var emp = new Event();
+
+// var count = 0;
+// function print() {
+//     count++;
+//     console.log('Click: ' + count);
+// }
+
+// emp.on('click', print);
+// emp.once('click', print);
+// emp.emit('click');
+// emp.emit('click');
+
 var Event = require('events').EventEmitter;
 
-var emp = new Event();
+var evt = new Event();
 
-var count = 0;
-var print = function () {
-    count++;
-    console.log('Click: ' + count);
-}
+evt.on('event', function () {
+    setTimeout(function () {
+        console.log('Listener 1');
+    }, 0);
+})
 
-emp.on('click', print);
-emp.emit('click');
+evt.on('event', function () {
+    setTimeout(function () {
+        console.log('Listener 2')
+    });
+})
+
+evt.on('event', function () {
+    console.log('Listener 3')
+})
+
+evt.emit('event');
