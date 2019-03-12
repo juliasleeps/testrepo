f618ed02eaff4107821a196405df239c37c22151
Yuka <cas_siopeia@live.com>
2019-03-12 21:05:00 +0200

    snth else

---
+++ 14/events/index.js
@@ -0,0 +1,12 @@
+var Event = require('events').EventEmitter;
+
+var emp = new Event();
+
+var count = 0;
+var print = function () {
+    count++;
+    console.log('Click: ' + count);
+}
+
+emp.on('click', print);
+emp.emit('print');
--- 14/index.js
+++ 14/index.js
@@ -1,10 +1,29 @@
+// var http = require('http');
+// var fs = require('fs');
+// var port = 3000;
+
+// http.createServer((request, response) => {
+//     response.write(fs.readFileSync('test.html'))
+//     response.end();
+// }).listen(port, function () {
+//     console.log('Server on http://localhost:3000')
+// }); 
+
 var http = require('http');
-var fs = require('fs');
 var port = 3000;
 
-http.createServer((request, response) => {
-    response.write(fs.readFileSync('test.html'))
-    response.end();
+http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World!')
+        res.end();
+    } else if (req.url === '/contact') {
+        res.write('Hello from Ukraine')
+        res.end();
+    } else {
+        res.write('Other')
+        res.end();
+    }
+
 }).listen(port, function () {
     console.log('Server on http://localhost:3000')
-}); 
+})
