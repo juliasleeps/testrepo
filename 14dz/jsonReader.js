5df1367d8ac61fde493dda8e8fc340285f32aaa2
Yuka <cas_siopeia@live.com>
2019-03-14 19:35:37 +0200

    add node request

--- 14dz/jsonReader.js
+++ 14dz/jsonReader.js
@@ -1,6 +1,13 @@
-var fs = require('fs');
+// var fs = require('fs');
 
-fs.readFile('currency.json', 'utf8', function(err, data){
-    const parsedData = JSON.parse(data);
-    console.log(parsedData);
+// fs.readFile('currency.json', 'utf8', function(err, data){
+//     const parsedData = JSON.parse(data);
+//     console.log(parsedData);
+// });
+
+const request = require('request');
+
+request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR1r_e2qbut-wyX9PIpa-D-Lw-vOvsCYQvW6Ls7lZhJGcVP4lYgEufOfJfs', { json: true }, (err, res, data) => {
+    if (err) { return console.log(err); }
+    console.log(data);
 });
